1e6468abc62aaa3550e8119cb401facba3ce566f
Momin Haider <h@momin.dev>
2023-09-24 18:39:04 -0700

    Agent rollouts for partially trained models

---
+++ test_offline_agent.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+agent_basename="system_default_normal"
+# define the range of values for the agent suffix
+start_val=0
+end_val=1000000
+step=10000
+
+# exactly one argument must be provided
+if [ $# -ne 1 ]; then
+    echo "ERROR: expected usage -> bash $0 <client_id>"
+    exit 1
+fi
+client_id=$1
+
+let "seed = $client_id + 56"
+
+# check if client_id is within valid range
+if [ $client_id -ge 0 ] && [ $client_id -le 7 ]; then
+    for ((i=start_val; i<=end_val; i+=step)); do
+        agent="$agent_basename.$(printf "%07d" $i)"
+        # python NetworkAgent/config_changer.py --test --agent $agent --seed $seed --steps 2000
+        # cd NetworkAgent/stable-baselines3
+        # python main_rl.py --env nqos_split --client_id $client_id
+        # cd ../..
+    done
+else
+    echo "ERROR: client_id must be an int between 0 and 7, inclusive. ($client_id provided)"
+    exit 1
+fi
