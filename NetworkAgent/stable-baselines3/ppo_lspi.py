fe8466c6c0829fb535313ff43c8a4a0232cc81df
Momin Haider <h@momin.dev>
2023-12-21 18:33:46 -0500

    Removed action parameterization
    - action parameterization was hurting the performance of the agents, because the linear features were selecting for the "extremes" in the action parameterization when doing the argmax

--- NetworkAgent/stable-baselines3/ppo_lspi.py
+++ NetworkAgent/stable-baselines3/ppo_lspi.py
@@ -18,7 +18,6 @@ class PPO_LSPI:
         self,
         model_name: str = "PPO",
         epsilon: float = 1.0e-3,
-        num_network_users: int = -1,
     ):
         this_dir = os.path.dirname(os.path.abspath(__file__))
         model_path = os.path.join(this_dir, "models", model_name)
@@ -28,39 +27,16 @@ class PPO_LSPI:
         self.gamma = 0.99
         self.epsilon = epsilon
         self.num_actions: int = int(agent.action_space.n)
-        # NOTE: num_network_users is a special parameter for NetworkGym environment
-        # it allows the actions to be featurized in a special way...
-        self.num_users = num_network_users
-        self.num_actions_per_user = int(round(self.num_actions ** (1 / self.num_users)))
         self.initialize_action_feature_map()
         self.store_parameters(agent.get_parameters()["policy"])
         self.initialize_Q_weights()
 
     def initialize_action_feature_map(self) -> None:
         action_indices = list(range(self.num_actions))
-        if self.num_users > 0:
-            action_feature_map = torch.zeros(
-                (self.num_actions, self.num_users), dtype=torch.float32, device="cuda:0"
-            )
-            for discrete_action in action_indices:
-                user_specific_action = get_user_discretized_actions(
-                    discrete_action, self.num_users, self.num_actions_per_user
-                )
-                tensor_user_action = torch.tensor(
-                    user_specific_action, dtype=torch.float32, device="cuda:0"
-                ).unsqueeze(0)
-                action_feature_map[discrete_action, :] = tensor_user_action
-            self.action_feature_map = action_feature_map.unsqueeze(0)
-            self.action_one_hots: torch.Tensor = F.one_hot(
-                torch.tensor(action_indices, device="cuda:0"),
-                num_classes=self.num_actions,
-            ).unsqueeze(0)
-        else:
-            self.action_feature_map: torch.Tensor = F.one_hot(
-                torch.tensor(action_indices, device="cuda:0"),
-                num_classes=self.num_actions,
-            ).unsqueeze(0)
-            self.action_one_hots = self.action_feature_map
+        self.action_one_hots: torch.Tensor = F.one_hot(
+            torch.tensor(action_indices, device="cuda:0"),
+            num_classes=self.num_actions,
+        ).unsqueeze(0)
 
     # NOTE: store the policy and value function parameters before the last layer.
     # need them for calculating featurization
@@ -76,13 +52,7 @@ class PPO_LSPI:
         self.vf_b2 = param_dict["mlp_extractor.value_net.2.bias"].unsqueeze(1)
 
     def initialize_Q_weights(self) -> None:
-        self.k = self.pi_b2.shape[0] + self.vf_b2.shape[0]
-        # NOTE: featurization of discrete action space is much smaller when using
-        # NetworkGym - saves time training
-        if self.num_users > 0:
-            self.k += self.num_users
-        else:
-            self.k += self.num_actions
+        self.k = self.pi_b2.shape[0] + self.vf_b2.shape[0] + self.num_actions
         self.w_tilde = torch.randn((self.k, 1), device="cuda:0")
 
     def store_to_buffer(
@@ -91,13 +61,10 @@ class PPO_LSPI:
         tensor_state = torch.tensor(
             state, dtype=torch.float32, device="cuda:0"
         ).unsqueeze(1)
-        if self.num_users > 0:
-            tensor_action = torch.tensor([action], device="cuda:0").unsqueeze(1)
-        else:
-            tensor_action: torch.Tensor = F.one_hot(
-                torch.tensor(action, device="cuda:0"),
-                num_classes=self.num_actions,
-            ).unsqueeze(1)
+        tensor_action: torch.Tensor = F.one_hot(
+            torch.tensor(action, device="cuda:0"),
+            num_classes=self.num_actions,
+        ).unsqueeze(1)
         tensor_reward = torch.tensor(
             [reward], dtype=torch.float32, device="cuda:0"
         ).unsqueeze(1)
@@ -127,7 +94,7 @@ class PPO_LSPI:
         phi_s_T = phi_s.T
         phi_s_repeated = phi_s_T.unsqueeze(1).repeat(1, self.num_actions, 1)
         L = phi_s.shape[1]
-        action_features_repeated = self.action_feature_map.repeat(L, 1, 1)
+        action_features_repeated = self.action_one_hots.repeat(L, 1, 1)
         phi_matrix = torch.cat([phi_s_repeated, action_features_repeated], dim=2)
         batch_w_tilde = self.w_tilde.unsqueeze(0).repeat(L, 1, 1)
         Q_values = torch.bmm(phi_matrix, batch_w_tilde)
@@ -165,21 +132,14 @@ class PPO_LSPI:
 
     def construct_phi_matrix(self) -> torch.Tensor:
         phi_s = self.compute_state_features(self.states)
-        action_indices = self.actions.squeeze(0)
-        action_features = torch.index_select(self.action_feature_map, 1, action_indices)
-        reshaped_action_features = action_features.squeeze(0).T
-        phi_matrix_transpose = torch.cat([phi_s, reshaped_action_features], dim=0)
+        phi_matrix_transpose = torch.cat([phi_s, self.actions], dim=0)
         phi_matrix = phi_matrix_transpose.T
         return phi_matrix
 
     def construct_phi_prime_matrix(self) -> torch.Tensor:
         phi_s_prime = self.compute_state_features(self.next_states)
-        pi_s_prime = self.policy(self.next_states, one_hot=False).squeeze(1)
-        policy_action_features = torch.index_select(
-            self.action_feature_map, 1, pi_s_prime
-        )
-        reshaped_policy_features = policy_action_features.squeeze(0)
-        phi_prime_matrix = torch.cat([phi_s_prime.T, reshaped_policy_features], dim=1)
+        pi_s_prime = self.policy(self.next_states, one_hot=True)
+        phi_prime_matrix = torch.cat([phi_s_prime.T, pi_s_prime], dim=1)
         return phi_prime_matrix
 
     # NOTE: incremental update of weight vector for Q-hat
@@ -203,13 +163,15 @@ class PPO_LSPI:
             # local suboptima...
             if torch.isnan(w_tilde).any():
                 rank_A_tilde = torch.linalg.matrix_rank(A_tilde)
-                print(f"A_tilde defective - rank {rank_A_tilde} < {A_tilde.shape[0]}")
+                print(
+                    f"A_tilde defective - rank {rank_A_tilde} < {A_tilde.shape[0]} - using random weights..."
+                )
                 norm_difference = 0
-                w_tilde = torch.randn((self.k, 1), device="cuda:0")
+                self.w_tilde = torch.randn((self.k, 1), device="cuda:0")
             else:
                 norm_difference = torch.norm(w_tilde - self.w_tilde, float("inf"))
-            self.w_tilde = w_tilde
-            print(f"||w - w'||_inf: {norm_difference}")
+                print(f"||w - w'||_inf: {norm_difference}")
+                self.w_tilde = w_tilde
             iteration += 1
 
     def get_Q_value(self, state: np.ndarray, action: int) -> float:
@@ -218,17 +180,17 @@ class PPO_LSPI:
             state, dtype=torch.float32, device="cuda:0"
         ).unsqueeze(1)
         phi_s = self.compute_state_features(tensor_state)
-        phi_a = self.action_feature_map[:, action, :].T
+        phi_a = self.action_one_hots[:, action, :].T
         phi_s_a = torch.cat([phi_s, phi_a], dim=0)
         Q_value = (phi_s_a.T @ self.w_tilde).item()
         return Q_value
 
 
 def main() -> None:
-    agent_thingy = PPO_LSPI(num_network_users=4)
+    agent_thingy = PPO_LSPI()
     random_state = np.random.random((14, 4))
     # testing generation of weights for Q-hat
-    for iter in range(1000):
+    for iter in range(2000):
         action, _ = agent_thingy.predict(random_state)
         print(f"iteration: {iter}, action: {action}")
         # NOTE: expect the action to eventually converge to 42, independent of state
