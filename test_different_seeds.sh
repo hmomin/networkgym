856ae8f5eec8d9131324a1afe5e8d9b78921a2ee
Momin Haider <h@momin.dev>
2024-02-10 20:57:51 -0800

    Updates
    - added ability to rerun script if it finishes too fast, because the
    worker restarts (gamechanger!)

--- test_different_seeds.sh
+++ test_different_seeds.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 agents=(
-    sys_default_norm_utility_bc.10000.64.0.625.not_normalized
-    sys_default_norm_utility_PTD3_beta_0.1_alpha_0.999_step_0010000
-    sys_default_norm_utility_PTD3_beta_0.1_alpha_0.9995_step_0010000
+    system_default
 )
 
 # exactly one argument must be provided
@@ -21,8 +19,19 @@ if [ $client_id -ge 0 ] && [ $client_id -le 7 ]; then
         echo "SEED: $seed --- AGENT: $agent"
         python -u NetworkAgent/config_changer.py --test --agent $agent --seed $seed --steps 3200
         cd NetworkAgent/stable-baselines3
+        # time python script time, to determine if it should be reran
+        start_time=$(date +%s)
         python -u main_rl.py --env nqos_split --client_id $client_id
+        end_time=$(date +%s)
+        elapsed_time=$((end_time - start_time))
         cd ../..
+        if [ $elapsed_time -lt 60 ]; then
+            echo "RERUNNING SCRIPT..."
+            python -u NetworkAgent/config_changer.py --test --agent $agent --seed $seed --steps 3200
+            cd NetworkAgent/stable-baselines3
+            python -u main_rl.py --env nqos_split --client_id $client_id
+            cd ../..
+        fi
     done
 else
     echo "ERROR: client_id must be an int between 0 and 7, inclusive. ($client_id provided)"
