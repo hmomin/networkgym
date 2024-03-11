8d563681e5d0db04ece4b2229fbed38f76068795
Momin Haider <h@momin.dev>
2024-03-11 08:48:32 -0700

    Updates
    - added testing for CQL model

--- NetworkAgent/stable-baselines3/main_rl.py
+++ NetworkAgent/stable-baselines3/main_rl.py
@@ -14,7 +14,7 @@ LOG_STD_MAX = 2.0
 
 import torch
 from torch import nn
-from torch.distributions import Normal
+from torch.distributions import Normal, TanhTransform, TransformedDistribution
 
 import sys
 
@@ -157,6 +157,149 @@ class LB_Actor(nn.Module):
         self, state: np.ndarray, deterministic: bool = True
     ) -> tuple[np.ndarray, None]:
         return (self.act(state, "cpu") + 1.0) / 2.0, None
+
+
+def init_module_weights(module: torch.nn.Sequential, orthogonal_init: bool = False):
+    # Specific orthgonal initialization for inner layers
+    # If orthogonal init is off, we do not change default initialization
+    if orthogonal_init:
+        for submodule in module[:-1]:
+            if isinstance(submodule, nn.Linear):
+                nn.init.orthogonal_(submodule.weight, gain=np.sqrt(2))
+                nn.init.constant_(submodule.bias, 0.0)
+
+    # Lasy layers should be initialzied differently as well
+    if orthogonal_init:
+        nn.init.orthogonal_(module[-1].weight, gain=1e-2)
+    else:
+        nn.init.xavier_uniform_(module[-1].weight, gain=1e-2)
+
+    nn.init.constant_(module[-1].bias, 0.0)
+
+
+class ReparameterizedTanhGaussian(nn.Module):
+    def __init__(
+        self,
+        log_std_min: float = -20.0,
+        log_std_max: float = 2.0,
+        no_tanh: bool = False,
+    ):
+        super().__init__()
+        self.log_std_min = log_std_min
+        self.log_std_max = log_std_max
+        self.no_tanh = no_tanh
+
+    def log_prob(
+        self, mean: torch.Tensor, log_std: torch.Tensor, sample: torch.Tensor
+    ) -> torch.Tensor:
+        log_std = torch.clamp(log_std, self.log_std_min, self.log_std_max)
+        std = torch.exp(log_std)
+        if self.no_tanh:
+            action_distribution = Normal(mean, std)
+        else:
+            action_distribution = TransformedDistribution(
+                Normal(mean, std), TanhTransform(cache_size=1)
+            )
+        return torch.sum(action_distribution.log_prob(sample), dim=-1)
+
+    def forward(
+        self, mean: torch.Tensor, log_std: torch.Tensor, deterministic: bool = False
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        log_std = torch.clamp(log_std, self.log_std_min, self.log_std_max)
+        std = torch.exp(log_std)
+
+        if self.no_tanh:
+            action_distribution = Normal(mean, std)
+        else:
+            action_distribution = TransformedDistribution(
+                Normal(mean, std), TanhTransform(cache_size=1)
+            )
+
+        if deterministic:
+            action_sample = torch.tanh(mean)
+        else:
+            action_sample = action_distribution.rsample()
+
+        log_prob = torch.sum(action_distribution.log_prob(action_sample), dim=-1)
+
+        return action_sample, log_prob
+
+
+class TanhGaussianPolicy(nn.Module):
+    def __init__(
+        self,
+        state_dim: int,
+        action_dim: int,
+        max_action: float,
+        log_std_multiplier: float = 1.0,
+        log_std_offset: float = -1.0,
+        orthogonal_init: bool = False,
+        no_tanh: bool = False,
+    ):
+        super().__init__()
+        self.observation_dim = state_dim
+        self.action_dim = action_dim
+        self.max_action = max_action
+        self.orthogonal_init = orthogonal_init
+        self.no_tanh = no_tanh
+
+        self.base_network = nn.Sequential(
+            nn.Linear(state_dim, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 2 * action_dim),
+        )
+
+        init_module_weights(self.base_network)
+
+        self.log_std_multiplier = Scalar(log_std_multiplier)
+        self.log_std_offset = Scalar(log_std_offset)
+        self.tanh_gaussian = ReparameterizedTanhGaussian(no_tanh=no_tanh)
+
+    def log_prob(
+        self, observations: torch.Tensor, actions: torch.Tensor
+    ) -> torch.Tensor:
+        base_network_output = self.base_network(observations)
+        mean, log_std = torch.split(base_network_output, self.action_dim, dim=-1)
+        log_std = self.log_std_multiplier() * log_std + self.log_std_offset()
+        _, log_probs = self.tanh_gaussian(mean, log_std, False)
+        return log_probs
+
+    def forward(
+        self,
+        observations: torch.Tensor,
+        deterministic: bool = False,
+        repeat: bool = None,
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        base_network_output = self.base_network(observations)
+        mean, log_std = torch.split(base_network_output, self.action_dim, dim=-1)
+        log_std = self.log_std_multiplier() * log_std + self.log_std_offset()
+        actions, log_probs = self.tanh_gaussian(mean, log_std, deterministic)
+        return self.max_action * actions, log_probs
+
+    @torch.no_grad()
+    def act(self, state: np.ndarray, device: str = "cpu"):
+        state = torch.tensor(state.reshape(1, -1), device=device, dtype=torch.float32)
+        with torch.no_grad():
+            actions, _ = self(state, not self.training)
+        return actions.cpu().data.numpy().flatten()
+
+    def predict(
+        self, state: np.ndarray, deterministic: bool = True
+    ) -> tuple[np.ndarray, None]:
+        return (self.act(state, "cpu") + 1.0) / 2.0, None
+
+
+class Scalar(nn.Module):
+    def __init__(self, init_value: float):
+        super().__init__()
+        self.constant = nn.Parameter(torch.tensor(init_value, dtype=torch.float32))
+
+    def forward(self) -> nn.Parameter:
+        return self.constant
 
 
 class Squeeze(nn.Module):
@@ -412,6 +555,16 @@ def main():
             state_dict = torch.load(open(model_path + ".pt", "rb"), "cuda:0")
             if rl_alg == "checkpoint_BC" or rl_alg == "checkpoint_TD3_BC":
                 agent = Actor(56, 4, 1.0)
+                mean_state = state_dict["state_mean"]
+                stdev_state = state_dict["state_std"]
+            elif rl_alg == "checkpoint_CQL":
+                agent = TanhGaussianPolicy(
+                    56,
+                    4,
+                    1.0,
+                    log_std_multiplier=1.0,
+                    orthogonal_init=True,
+                )
                 mean_state = state_dict["state_mean"]
                 stdev_state = state_dict["state_std"]
             elif rl_alg == "checkpoint_IQL":
