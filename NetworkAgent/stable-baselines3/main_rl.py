4a743b7ac676aa92ef5751dcc83bca6d88c7a9d3
Momin Haider <h@momin.dev>
2024-03-11 08:55:50 -0700

    Updates
    - added testing for trained EDAC model

--- NetworkAgent/stable-baselines3/main_rl.py
+++ NetworkAgent/stable-baselines3/main_rl.py
@@ -293,6 +293,75 @@ class TanhGaussianPolicy(nn.Module):
         return (self.act(state, "cpu") + 1.0) / 2.0, None
 
 
+class EDAC_Actor(nn.Module):
+    def __init__(
+        self, state_dim: int, action_dim: int, hidden_dim: int, max_action: float = 1.0
+    ):
+        super().__init__()
+        self.trunk = nn.Sequential(
+            nn.Linear(state_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+        )
+        # with separate layers works better than with Linear(hidden_dim, 2 * action_dim)
+        self.mu = nn.Linear(hidden_dim, action_dim)
+        self.log_sigma = nn.Linear(hidden_dim, action_dim)
+
+        # init as in the EDAC paper
+        for layer in self.trunk[::2]:
+            torch.nn.init.constant_(layer.bias, 0.1)
+
+        torch.nn.init.uniform_(self.mu.weight, -1e-3, 1e-3)
+        torch.nn.init.uniform_(self.mu.bias, -1e-3, 1e-3)
+        torch.nn.init.uniform_(self.log_sigma.weight, -1e-3, 1e-3)
+        torch.nn.init.uniform_(self.log_sigma.bias, -1e-3, 1e-3)
+
+        self.action_dim = action_dim
+        self.max_action = max_action
+
+    def forward(
+        self,
+        state: torch.Tensor,
+        deterministic: bool = False,
+        need_log_prob: bool = False,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
+        hidden = self.trunk(state)
+        mu, log_sigma = self.mu(hidden), self.log_sigma(hidden)
+
+        # clipping params from EDAC paper, not as in SAC paper (-20, 2)
+        log_sigma = torch.clip(log_sigma, -5, 2)
+        policy_dist = Normal(mu, torch.exp(log_sigma))
+
+        if deterministic:
+            action = mu
+        else:
+            action = policy_dist.rsample()
+
+        tanh_action, log_prob = torch.tanh(action), None
+        if need_log_prob:
+            # change of variables formula (SAC paper, appendix C, eq 21)
+            log_prob = policy_dist.log_prob(action).sum(axis=-1)
+            log_prob = log_prob - torch.log(1 - tanh_action.pow(2) + 1e-6).sum(axis=-1)
+
+        return tanh_action * self.max_action, log_prob
+
+    @torch.no_grad()
+    def act(self, state: np.ndarray, device: str) -> np.ndarray:
+        state = state.flatten()
+        deterministic = True
+        state = torch.tensor(state, device=device, dtype=torch.float32)
+        action = self(state, deterministic=deterministic)[0].cpu().numpy()
+        return action
+
+    def predict(
+        self, state: np.ndarray, deterministic: bool = True
+    ) -> tuple[np.ndarray, None]:
+        return (self.act(state, "cpu") + 1.0) / 2.0, None
+
+
 class Scalar(nn.Module):
     def __init__(self, init_value: float):
         super().__init__()
@@ -573,6 +642,8 @@ def main():
                 stdev_state = state_dict["state_std"]
             elif rl_alg == "checkpoint_LB-SAC":
                 agent = LB_Actor(56, 4, 256, False, 1.0)
+            elif rl_alg == "checkpoint_EDAC":
+                agent = EDAC_Actor(56, 4, 256, 1.0)
             else:
                 raise Exception("haven't implemented this actor type yet...")
             agent.load_state_dict(state_dict["actor"])
