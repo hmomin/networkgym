11e3fc6d45111688542c770d50999ee4e4df939a
Momin Haider <h@momin.dev>
2024-03-14 09:33:54 -0700

    Updates
    - modified online training in parallel to use already created PPO agent
    - this allows training with reloaded models (probably a better way to do this though)

--- train_online_parallel.sh
+++ train_online_parallel.sh
@@ -3,7 +3,7 @@
 # NOTE: trains PPO and SAC on parallel envs
 
 echo "Cleaning up broken environments"
-python -u NetworkAgent/config_changer.py --train --agent DDPG --seed 0 --steps 2
+python -u NetworkAgent/config_changer.py --test --agent PPO --seed 0 --steps 2
 cd NetworkAgent/stable-baselines3
 python -u main_rl.py --env nqos_split --client_id 0
 python -u main_rl.py --env nqos_split --client_id 1
@@ -16,13 +16,13 @@ python -u main_rl.py --env nqos_split --client_id 7
 cd ../..
 
 echo "Training PPO"
-python -u NetworkAgent/config_changer.py --train --parallel_env --agent PPO --seed 0 --steps 102000
+python -u NetworkAgent/config_changer.py --train --parallel_env --agent PPO --seed 16 --steps 80000
 cd NetworkAgent/stable-baselines3
 python -u main_rl.py --env nqos_split --client_id 0
 cd ../..
 
 echo "Cleaning up broken environments"
-python -u NetworkAgent/config_changer.py --train --agent DDPG --seed 0 --steps 2
+python -u NetworkAgent/config_changer.py --test --agent PPO --seed 0 --steps 2
 cd NetworkAgent/stable-baselines3
 python -u main_rl.py --env nqos_split --client_id 0
 python -u main_rl.py --env nqos_split --client_id 1
@@ -35,7 +35,7 @@ python -u main_rl.py --env nqos_split --client_id 7
 cd ../..
 
 echo "Training SAC"
-python -u NetworkAgent/config_changer.py --train --parallel_env --agent SAC --seed 0 --steps 102400
+python -u NetworkAgent/config_changer.py --train --parallel_env --agent SAC --seed 16 --steps 80000
 cd NetworkAgent/stable-baselines3
 python -u main_rl.py --env nqos_split --client_id 0
 cd ../..
